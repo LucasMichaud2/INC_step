580173ba2b5516c2fdc0f0cf2835768a28e24c24
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-01 17:15:14 +0400

    Update step.py

--- step.py
+++ step.py
@@ -6,5 +6,7 @@ st.set_page_config(layout='wide')
 logo_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/INC_Step_Challenge.jpg'
 name_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/namelist.csv'
 df_name = pd.read_csv(name_url)
+col_drop = ['Unamed: 2', 'Unamed: 3', 'Unamed: 4', 'Unamed: 5']
+df_name = df_name.drop(columns=col_drop)
 st.image(logo_url)
 st.dataframe(df_name)

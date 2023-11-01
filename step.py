a6b74000dd13ad821db62484e9761f85fc55f574
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-01 17:13:29 +0400

    Update step.py

--- step.py
+++ step.py
@@ -4,4 +4,7 @@ import streamlit as st
 st.set_page_config(layout='wide')
 
 logo_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/INC_Step_Challenge.jpg'
+name_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/namelist.csv'
+df_name = pd.read_csv(name_url)
 st.image(logo_url)
+st.dataframe(df_name)

e6408c9f54c1504dc1b89577ffde239d4018c002
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-01 16:58:23 +0400

    Update step.py

--- step.py
+++ step.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import streamlit as st
 
+st.set_page_config(layout='wide')
 
-logo_url = 
+logo_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/step.py'
+st.image(logo_url)

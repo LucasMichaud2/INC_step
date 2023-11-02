9365f94c7af7a9dca9ee0db9c8d6f619e5b49e17
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-02 17:54:13 +0400

    Update step.py

--- step.py
+++ step.py
@@ -5,21 +5,12 @@ import numpy as np
 st.set_page_config(layout='wide')
 
 logo_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/INC_Step_Challenge.jpg'
-name_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/namelist.csv'
+name_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/office_steps.csv'
 df_name = pd.read_csv(name_url)
-col_drop = ['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4', 'Unnamed: 5']
-df_name = df_name.drop(columns=col_drop)
 st.image(logo_url)
 
-df_office = df_name[['OFFICE']].copy()
-df_office['Steps'] = 0
-df_site = df_name[['On site']].copy()
-df_site = df_site.head(3)
-df_site['Steps'] = 0
-st.dataframe(df_office)
-st.dataframe(df_site)
 
-office_list = df_office['OFFICE'].tolist()
+st.dataframe(df_name)
 
 
 col1, col2 = st.columns(2)

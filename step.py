eab55dfafb51236f6e014a750aaca63efc2e988c
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-02 18:31:37 +0400

    Update step.py

--- step.py
+++ step.py
@@ -20,41 +20,26 @@ with col1:
 
   st.subheader('Office')
 
-  col11, col12 = st.columns(2)
+  with st.form("form1"):
 
-  with col11:
-    name_select = st.selectbox('Your name', office_name)
-
-  with col12:
-    daily_steps = st.number_input('Daily steps')
-
-
-  date = st.date_input('select date')
+    col11, col12 = st.columns(2)
+  
+    with col11:
+      name_select = st.selectbox('Your name', office_name)
+  
+    with col12:
+      daily_steps = st.number_input('Daily steps')
+  
+  
+    date = st.date_input('select date')
+    submitted = st.form_submit_button("Submit")
+    
 
 st.write(date)
 
-
-
-# Create a form using st.form
-with st.form("my_form"):
-    # Add a text input field for the user to enter a name
-    name = st.text_input("Enter your name")
-
-    # Add a submit button
-    submitted = st.form_submit_button("Submit")
-
-# Check if the "Submit" button was clicked
 if submitted:
-    # Display a message with the submitted name
-    st.write(f"Hello, {name}!")
-
-# You can also add more form elements here if needed
-
-# Display the result
-
-
   
 
-df_office.loc[df_office['Office'] == name_select, date] = daily_steps
-  
-st.dataframe(df_office)
+  df_office.loc[df_office['Office'] == name_select, date] = daily_steps
+    
+  st.dataframe(df_office)

48e191cc124f1a4af6e633b913aa3c3926ffd847
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-02 19:25:54 +0400

    Update step.py

--- step.py
+++ step.py
@@ -4,6 +4,8 @@ import numpy as np
 
 st.set_page_config(layout='wide')
 
+token = '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'
+
 logo_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/INC_Step_Challenge.jpg'
 name_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/office_steps.csv'
 df_office = pd.read_csv(name_url)

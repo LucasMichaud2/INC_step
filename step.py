2036e59c7e8901e0c1496182858e19da9d07893c
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-06 14:49:59 +0400

    Update step.py

--- step.py
+++ step.py
@@ -1,82 +1,44 @@
-import pandas as pd
-import streamlit as st
-import numpy as np
+import os
 import requests
+import pandas as pd
 
+# GitHub repository information
+owner = "LucasMichaud2"
+repo = "INC_step"
+file_path = "main/office_steps.csv"
 
-st.set_page_config(layout='wide')
+# Read and update the CSV data
+df = pd.read_csv(file_path)
 
-token = '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'
-
-logo_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/INC_Step_Challenge.jpg'
-name_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/office_steps.csv'
-df_office = pd.read_csv(name_url)
-st.image(logo_url)
-office_name = df_office['Office'].tolist()
-
-
-st.dataframe(df_office) 
-
-
-col1, col2 = st.columns(2)
-
-with col1:
-
-  st.subheader('Office')
-
-  with st.form("form1"):
-
-    col11, col12 = st.columns(2)
-  
-    with col11:
-      name_select = st.selectbox('Your name', office_name)
-  
-    with col12:
-      daily_steps = st.number_input('Daily steps')
-  
-  
-    date = st.date_input('select date')
-    submitted = st.form_submit_button("Submit")
-    
-
-st.write(date)
+with st.form("form1"):
+# Your update logic here
+  new_value = st.number_input('heelo')
+  df['Steps'] = new_value
+  submitted = st.form_submit_button("Submit")
 
 if submitted:
+
+  # Convert DataFrame back to CSV
+  updated_csv = df.to_csv(index=False)
   
-
-  df_office.loc[df_office['Office'] == name_select, date] = daily_steps
-
-  csv_data = df_office.to_csv()
-
+  # GitHub API endpoint
+  url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}"
+  
+  # Prepare the request headers
   headers = {
-    "Authorization": "token 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",
-    "Content-Type": "application/json",
+      "Authorization": f"Bearer {os.environ['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']}",
   }
-
-  sha_url = "https://api.github.com/repos/LucasMichaud2/INC_step/contents/office_steps.csv"
-  sha_response = 200 #requests.get(sha_url, headers=headers)
-  st.write(sha_response)
-
-  if sha_response.status_code == 200:
-    sha = sha_response.json().get("sha")
-
-    payload = {
-      "message": "Update CSV file",
-      "content": csv_data,
-      "sha": "peuneogJjUumOnbZdb52zZbp10ZbcrDYwyyUZ+7hE1Y",
-    }
-
-    update_url = "https://api.github.com/repos/LucasMichaud2/INC_step/contents/office_steps.csv"
-    response = requests.put(update_url, headers=headers,  json=payload)
-
-
-    if response.status_code == 200:
-      st.success("CSV file updated succesfully on Github!")
   
-    else:
-      st.error("Failed to update CSV file on Github.")
-
+  # Prepare the request data
+  data = {
+      "message": "Update CSV file",
+      "content": updated_csv,
+  }
+  
+  # Send the PUT request to update the file
+  response = requests.put(url, headers=headers, json=data)
+  
+  if response.status_code == 200:
+      print("CSV file updated successfully on GitHub!")
   else:
-    st.error("Failed retrieve Sha")
-
-st.dataframe(df_office)
+      print(f"Failed to update CSV file on GitHub. Status code: {response.status_code}")

ae34b5dab15b21b4dc8e85e45617579846ad62ff
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-07 19:32:38 +0400

    Update step.py

--- step.py
+++ step.py
@@ -11,6 +11,7 @@ token = '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'
 logo_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/INC_Step_Challenge.jpg'
 name_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/office_steps.csv'
 df_office = pd.read_csv(name_url)
+df_office = pd.DataFrame(df_office)
 st.image(logo_url)
 office_name = df_office['Office'].tolist()
 st.dataframe(df_office) 

331f3780b10bcdf2568508b8d0c43dcef3bfdbdc
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-07 19:00:12 +0400

    Update step.py

--- step.py
+++ step.py
@@ -1,49 +1,18 @@
-import os
-import requests
 import pandas as pd
 import streamlit as st
-
-# GitHub repository information
-owner = "LucasMichaud2"
-repo = "INC_step"
-file_path = "https://raw.githubusercontent.com/LucasMichaud2/INC_step/main/office_steps.csv"
+import numpy as np
+import requests
 
 
-# Read and update the CSV data
-df = pd.read_csv(file_path)
+st.set_page_config(layout='wide')
 
-github_token = os.environ.get('GITHUB_TOKEN')
-print(f"GITHUB_TOKEN value: {github_token}")
+token = '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'
 
-with st.form("form1"):
-# Your update logic here
-  new_value = st.number_input('heelo')
-  df['Steps'] = new_value
-  submitted = st.form_submit_button("Submit")
+logo_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/INC_Step_Challenge.jpg'
+name_url = 'https://raw.github.com/LucasMichaud2/INC_step/main/office_steps.csv'
+df_office = pd.read_csv(name_url)
+st.image(logo_url)
+office_name = df_office['Office'].tolist()
 
-if submitted:
 
-  # Convert DataFrame back to CSV
-  updated_csv = df.to_csv(index=False)
-  
-  # GitHub API endpoint
-  url = f"https://api.github.com/repos/{owner}/{repo}/contents/office_steps.csv"
-  
-  # Prepare the request headers
-  headers = {
-      "Authorization": f"Bearer {os.environ['TOKEN']}",
-  }
-  
-  # Prepare the request data
-  data = {
-      "message": "Update CSV file",
-      "content": updated_csv,
-  }
-  
-  # Send the PUT request to update the file
-  response = requests.put(url, headers=headers, json=data)
-  
-  if response.status_code == 200:
-      print("CSV file updated successfully on GitHub!")
-  else:
-      print(f"Failed to update CSV file on GitHub. Status code: {response.status_code}")
+st.dataframe(df_office) 

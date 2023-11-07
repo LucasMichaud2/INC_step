89e0d97ffb5bef75a18bf25d3f7eaaf0b9cb656c
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-07 19:18:57 +0400

    Update step.py

--- step.py
+++ step.py
@@ -14,5 +14,12 @@ df_office = pd.read_csv(name_url)
 st.image(logo_url)
 office_name = df_office['Office'].tolist()
 
+col_to_drop = ['Office', 'Steps']
+temp_data = df_office.drop(col_to_drop)
+office_board = df_office['Office']
+office_board['Steps'] = temp_data.sum(axis=1)
+
 
 st.dataframe(df_office) 
+
+st.dataframe(office_board)

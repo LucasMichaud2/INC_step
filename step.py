42aba05c13a77e49d7f5c0d34b41964c8ccd2f91
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-08 19:41:15 +0400

    Update step.py

--- step.py
+++ step.py
@@ -19,6 +19,9 @@ def formatting(url):
  office_name = df_office['Office'].tolist()
  
  office_board = df_office[['Office', 'Steps']]
+ one_day = df_office.drop('Steps', axis=1)
+ one_day['Daily Steps'] = one_day.iloc[:, 1].max(axis=1)
+ one_day = df[['Office', 'Daily Steps']]
  temp_data = df_office.copy()
  col_to_drop = ['Office', 'Steps']
  temp_data = temp_data.drop(columns=col_to_drop)
@@ -31,10 +34,14 @@ def formatting(url):
  office_board = office_board.sort_values(by='Steps', ascending=False)
  office_board = office_board.reset_index(drop=True)
  office_board.index = range(1, len(office_board) + 1)
- return office_board
+ one_day = one_day.sort_values(by='Steps', ascending=False)
+ one_day = one_day.reset_index(drop=True)
+ one_day.index = range(1, len(one_day) + 1)
+ 
+ return office_board, one_day
 
-office_board = formatting(name_url)
-onsite_board = formatting(name_url1)
+office_board, office_day = formatting(name_url)
+onsite_board, onsite_day = formatting(name_url1)
 
 
 col1, col2 = st.columns(2)
@@ -49,6 +56,10 @@ with col1:
        
   st.dataframe(office_board)
 
+ with col12:
+  st.write('Most Steps in a Day')
+  st.dataframe(office_day)
+
 with col2:
  st.subheader('On Site', divider='grey')
  col21, col22 = st.columns(2)
@@ -57,4 +68,8 @@ with col2:
   
        
   st.dataframe(onsite_board)
+
+ with col22:
+  st.write('Most Steps in a Day')
+  st.dataframe(onsite_day)
  
